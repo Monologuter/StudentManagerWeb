88272fc95fbc6e9d9b2533803d2588e53d6756f8
cyy <2439644676@qq.com>
2020-10-29 15:01:25 +0800

    第一次提交

---
+++ src/com/ischoolbar/programmer/model/Student.java
@@ -0,0 +1,96 @@
+package com.ischoolbar.programmer.model;
+
+import java.io.InputStream;
+
+/**
+ * 
+ * @author llq
+ *学生实体类
+ */
+public class Student {
+	private int id; //编号 自增长 
+	private String sn;//学号
+	private String name; //姓名
+	private String password; //密码 
+	private int clazzId; //班级
+	private String sex = "男"; //性别
+	private String mobile;  //手机号码
+	private String qq; //qq
+	private InputStream photo;//头像
+	private String height; //学生身高
+	private String weight; //学生体重
+	private String description; //老师评语
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSn() {
+		return sn;
+	}
+	public void setSn(String sn) {
+		this.sn = sn;
+	}
+	public int getClazzId() {
+		return clazzId;
+	}
+	public void setClazzId(int clazzId) {
+		this.clazzId = clazzId;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public InputStream getPhoto() {
+		return photo;
+	}
+	public void setPhoto(InputStream photo) {
+		this.photo = photo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
